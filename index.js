f6802155203cfcb8ce9f42510602f0d5b4568346
Marylucy <marylucyatienoomenda.gmail.com>
2022-11-21 11:48:04 -0500

    complited assignment

---
+++ index.js
@@ -0,0 +1,24 @@
+function shout(hello) {
+    return hello.toUpperCase();
+  }
+  function whisper(hello) {
+    return hello.toLowerCase();
+  }
+  function logShout (hello) {
+  console .log(hello.toUpperCase());
+  }
+function logWhisper (hello){
+console .log(hello.toLowerCase())
+}
+
+function sayHiToHeadphonedRoommate(string){
+ if(string==="Let\'s have dinner together!"){
+return "I would love to!" ;
+ }
+else if(string=== string.toUpperCase()){
+return "YES INDEED!" ;
+}
+else if (string===string.toLowerCase());{
+return "I can\'t hear you!"
+}
+}
